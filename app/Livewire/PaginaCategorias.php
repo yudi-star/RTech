8c677f096a37bd8206626876fe255d8e73244750
mathias-villena <mathias.villena@tecsup.edu.pe>
2024-11-21 16:04:54 -0500

    Avanze de relacion de productos y categorias con la pagina principal

--- app/Livewire/PaginaCategorias.php
+++ app/Livewire/PaginaCategorias.php
@@ -1,13 +1,16 @@
 <?php
-
 namespace App\Livewire;
-
+use App\Models\Categoria;
+use Livewire\Attributes\Title;
 use Livewire\Component;
-
+#[Title('Categorias - RebornTech')]
 class PaginaCategorias extends Component
 {
     public function render()
     {
-        return view('livewire.pagina-categorias');
+        $categorias = Categoria::where('activo', 1)->get();
+        return view('livewire.pagina-categorias',[
+            'categorias'=>$categorias,
+        ]);
     }
 }
--- app/Livewire/PaginaInicio.php
+++ app/Livewire/PaginaInicio.php
@@ -1,13 +1,21 @@
 <?php
 
 namespace App\Livewire;
-
+use App\Models\Categoria;
+use App\Models\Marca;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Pagina Inicio - RebornTech')]
 class PaginaInicio extends Component
 {
     public function render()
     {
-        return view('livewire.pagina-inicio');
+        $marcas = Marca::where('activo', 1)->get();
+        $categorias = Categoria::where('activo',1)->get();
+        return view('livewire.pagina-inicio',[
+            'marcas'=> $marcas,
+            'categorias'=>$categorias,
+        ]);
     }
 }
