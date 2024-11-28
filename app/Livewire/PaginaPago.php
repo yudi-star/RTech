4d91be42aea682647cf5ff5bdc1cbf9a5e3d4841
Thecabezon <jeffersonrojassosa150@gmail.com>
2024-11-27 20:55:43 -0500

    formulario de finalizar compra casi terminado (21)

--- app/Livewire/PaginaPago.php
+++ app/Livewire/PaginaPago.php
@@ -3,11 +3,56 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\Attributes\Title;
+use App\Helpers\CarritoGestion;
 
-class PaginaPago extends Component
-{
-    public function render()
-    {
-        return view('livewire.pagina-pago');
+#[Title('Pago - Reborntech')]
+class PaginaPago extends Component{
+
+    public $nombre;
+    public $apellido;
+    public $telefono;
+    public $direccion;
+    public $ciudad;
+    public $distrito;
+    public $codigo_postal;
+    public $metodo_pago;
+
+
+    public function confirmarPedido(){
+        $this->validate([
+            'nombre' => 'required',
+            'apellido' => 'required',
+            'telefono' => 'required',
+            'direccion' => 'required',
+            'ciudad' => 'required',
+            'distrito' => 'required',
+            'codigo_postal' => 'required',
+            'metodo_pago' => 'required',
+        ]);
+    }
+
+    public function messages(){
+        return [
+            'nombre.required' => 'Debes de ingresar tu nombre',
+            'apellido.required' => 'Debes de ingresar tu apellido',
+            'telefono.required' => 'Debes de ingresar tu telefono',
+            'direccion.required' => 'Debes de ingresar tu direccion',
+            'ciudad.required' => 'Debes de ingresar tu ciudad',
+            'distrito.required' => 'Debes de ingresar tu distrito',
+            'codigo_postal.required' => 'Debes de ingresar tu codigo postal',
+            'metodo_pago.required' => 'Debes de seleccionar un metodo de pago',
+        ];
+    }
+
+    public function render(){
+
+        $items_carrito = CarritoGestion::obtenerTotalArticulosDeCookie();
+        $total = CarritoGestion::obtenerPrecioTotalCarrito($items_carrito);
+
+        return view('livewire.pagina-pago', [
+            'items_carrito' => $items_carrito,
+            'total' => $total
+        ]);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,9 +6,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
-class User extends Authenticatable
-{
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
+class User extends Authenticatable implements FilamentUser{
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
@@ -49,5 +49,10 @@ class User extends Authenticatable
 
     public function ordenes(){
         return $this->hasMany(Orden::class);
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->email === 'admin123@gmail.com';
     }
 }
